7a9b150453e72ffe43216f4a88b7466b5f6b5a64
asnganje <abd.nganje@gmail.com>
2025-12-13 13:44:33 +0300

    add logic for login and logout

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::SessionsController < Devise::RegistrationsController
+  respond_to :json
+  before_action :authenticate_user!, only: [ :destroy ]
+  def create
+    user_params = params.require(:user).permit(:email, :password)
+    user = User.find_by(email: user_params[:email])
+
+    if user&.valid_password?(user_params[:password])
+      token = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first
+
+      render json: {
+        msg: "success login",
+        token: token,
+        user: {
+          id: user.id,
+          email: user.email,
+          fullname: user.fullname
+        }
+      },
+      status: :ok
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    request.env["warden"].logout(:user)
+    render json: { msg: "Logged out successfully" }, status: :ok
+  end
+end
