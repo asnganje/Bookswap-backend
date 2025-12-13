0dd3219b299aa1b9a91ebd37e4a4d755ffabbb5a
asnganje <abd.nganje@gmail.com>
2025-12-13 13:44:21 +0300

    add logic for user signup

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  def create
+    user = User.new(sign_up_params)
+    if user.save
+      render json: { msg: "Registration success", user: user }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def sign_up_params
+    params.require(:user).permit(:fullname, :email, :password)
+  end
+end
