c8ffc2efb24b706b3ba6a18f1d42b29a25cc9475
asnganje <abd.nganje@gmail.com>
2026-01-05 18:21:35 +0300

    modify code setup to handle book image from amazon s3

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,7 @@ class Book < ApplicationRecord
   validates :title, :author, :genre, presence: true
 
   def image_url
-    Rails.application.routes.url_helpers.url_for(image) if image.attached?
+    return unless image.attached?
+    image.url
   end
 end
